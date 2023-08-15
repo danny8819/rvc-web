5413aed97bbe2e0e0c7b47e9568b2a593d630b58
xxxsjan <626653354@qq.com>
2023-08-15 21:31:33 +0800

    feat:配置pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 import 'normalize.css'
 import 'ress'
@@ -12,7 +13,6 @@ import './style/scss/index.scss'
 import { createVuetify } from 'vuetify'
  
 const vuetify = createVuetify({
- 
   theme:{
     themes:{
        light:{
@@ -29,7 +29,22 @@ const vuetify = createVuetify({
           variables:{}
        }
     }
+  },
+  display:{
+    thresholds:{
+        // "xs": 0,
+        // "sm": 600, // min
+        // "md": 960,
+        // "lg": 1280,
+        // "xl": 1920,
+        // "xxl": 2560
+    }
   }
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
