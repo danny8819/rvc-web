f65e3e0a8b7ba098db929ed3d57460b78d377cee
xxxsjan <626653354@qq.com>
2023-08-26 13:00:45 +0800

    feat:svg-icon 组件

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-
+import 'virtual:svg-icons-register';
 import 'normalize.css'
 import 'ress'
 import 'vuetify/styles' // 原子样式可参考 vuetify\lib\styles\main.css
@@ -15,43 +15,27 @@ import './style/scss/index.scss'
 import 'element-plus/dist/index.css'
 import 'jquery'
 
-import { createVuetify } from 'vuetify'
+import vuetify from './plugins/vuetify'
+import vueFilePond from "vue-filepond";
+import SvgIcon from './components/SvgIcon.vue'
 
-const vuetify = createVuetify({
-  theme:{
-    themes:{
-       light:{
-          dark:false,
-          colors:{
-      //       primary:'#316ce8',
-      //       accent:'#607df9',
-      //       secondary:'#a73aeb',
-      //       info:'#0164d3',
-      //       warning:'#fec500',
-            error:'#ff4d7e',
-      //       success:'#2cdd9b',
-          },
-          variables:{}
-       }
-    }
-  },
-  display:{
-    thresholds:{
-        // "xs": 0,
-        // "sm": 600, // min
-        // "md": 960,
-        // "lg": 1280,
-        // "xl": 1920,
-        // "xxl": 2560
-    }
-  }
-})
+import "filepond/dist/filepond.min.css";
+// import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+// import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+// import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 
+const FilePond = vueFilePond(
+  // FilePondPluginFileValidateType,
+  // FilePondPluginImagePreview
+);
+
+console.log(FilePond);
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-
+app.component('file-pond', FilePond)
+app.component('file-pond', SvgIcon)
 app.use(ElementPlus)
 .use(store)
 .use(router)
