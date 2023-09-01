5f033dc01892a14bc927e6321d42760280f29f96
xxxsjan <626653354@qq.com>
2023-09-01 18:11:47 +0800

    feat:默认主题色

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,17 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import SvgIcon from './components/SvgIcon.vue'
+import 'virtual:svg-icons-register';
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'jquery'
 
 import 'ress'
 import './style/main.css' // 原子样式 源 vuetify\lib\styles\main.css
-
 import '@mdi/font/css/materialdesignicons.css'
 
 import './style/scss/index.scss'
-
-// import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
- 
 import './style/index.scss'
-import 'virtual:svg-icons-register';
 
 const app = createApp(App)
 
@@ -29,7 +24,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 
 app.component('SvgIcon', SvgIcon)
 
-// app.use(ElementPlus)
 app.use(store)
 .use(router)
 .use(vuetify)
