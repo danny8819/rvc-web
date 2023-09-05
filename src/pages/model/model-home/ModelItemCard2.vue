d1f2026ed1b0a0a8da43091b690a9ba1244e20ec
xxxsjan <626653354@qq.com>
2023-09-06 01:00:47 +0800

    chore 模型首页调整

---
+++ src/pages/model/model-home/ModelItemCard2.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="model-item">
+    <div class="model-item__pic--wrap">
+      <img
+        src="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/3c451176-a098-4191-907a-e898bfdf673b/width=2048/ComfyUI_00747_.jpeg"
+      />
+    </div>
+    <div class="model-item__footer">
+      <div class="model-item__title">
+        【原神】全任务全剧情1 序章 第一章 完成 捕风的异乡人 PC版 Yuan Shen
+        x天马骑士x版
+      </div>
+      <div class="d-flex justify-space-between">
+        <div class="d-flex align-center">
+          <el-avatar :size="30" :src="data.avatar"> </el-avatar>
+          <span class="ml-2">{{ data.nickname }}</span>
+        </div>
+        <div class="d-flex justify-space-around align-center">
+          <SvgIcon name="liulan"></SvgIcon>
+          {{ data.lookNum }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  data: Object,
+});
+</script>
+
+<style scoped lang="scss">
+.model-item {
+  position: relative;
+
+  width: 24%;
+  max-width: 400px;
+
+  box-sizing: border-box;
+  margin-bottom: 5px;
+  margin-right: 10px;
+  transition: all 0.2s ease-out;
+  border-radius: 5px;
+
+  overflow: hidden;
+  .model-item__pic--wrap {
+    transform: scale(1.1);
+    transition: all 0.2s ease-out;
+  }
+  &:hover {
+    transform: translateY(2px);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.05);
+
+    .model-item__pic--wrap {
+      transform: scale(1.1);
+    }
+  }
+}
+
+.model-item__pic--wrap {
+  overflow: hidden;
+  position: relative;
+  padding-top: 150%;
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 1;
+    border-radius: 6px;
+    overflow: hidden;
+  }
+}
+
+.model-item__footer {
+  font-size: 13px;
+  color: #9499a0;
+  padding: 5px;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  position: absolute;
+  bottom: 0;
+  z-index: 2;
+
+  .model-item__title {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    font-size: 14px;
+    line-height: 1.5;
+    max-height: 1.5em;
+    padding-right: 16px;
+    margin: 10px 0;
+  }
+}
+</style>
--- src/pages/model/model-home/index.vue
+++ src/pages/model/model-home/index.vue
@@ -1,55 +1,53 @@
 <template>
-  <div class="container-1200">
-    <el-row class="mx-5 my-5" :gutter="20">
-      <el-col :span="18">
-        <el-card>
-          <el-carousel :interval="5000" arrow="always">
-            <el-carousel-item v-for="item in 4" :key="item">
-              <el-image
-                style="width: 100%; height: 100%"
-                src="https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png"
-                fit="contain"
-              />
-            </el-carousel-item>
-          </el-carousel>
-        </el-card>
-      </el-col>
+  <div class="model-home container-1200">
+    <el-card class="carousel-wrap mb-5">
+      <el-carousel :interval="5000" arrow="always">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <el-image
+            style="width: 100%; height: 100%"
+            src="https://img0.baidu.com/it/u=3604177901,3426946885&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"
+            fit="cover"
+          />
+        </el-carousel-item>
+      </el-carousel>
+    </el-card>
 
-      <el-col :span="6">
-        <el-card
-          class="rounded-lg h-100 d-flex flex-column justify-space-between align-center"
-        >
-          <div class="publish-btn" @click="$router.push('/model-publish')">
-            <SvgIcon name="upload" color="#fff" />
-            <span class="ml-2">发布模型</span>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-    <div class="d-flex flex-wrap">
-      <ModelItemCard
-        v-for="(item, index) in modelList"
-        :key="index"
-        @click="handleToDetail(item)"
-        :data="item"
-      />
+    <el-card class="h-100 mb-5">
+      <div class="d-flex justify-end">
+        <div class="publish-btn" @click="$router.push('/model-publish')">
+          <SvgIcon name="upload" color="#fff" />
+          <span class="ml-2">发布模型</span>
+        </div>
+      </div>
+    </el-card>
+
+    <div class="model-home-list">
+      <template v-for="(i, ff) in 11" :key="ff">
+        <ModelItemCard
+          v-for="(item, index) in modelList"
+          :key="index"
+          @click="handleToDetail(item)"
+          :data="item"
+        />
+      </template>
     </div>
 
     <div class="model-home__pagination d-flex justify-center">
-      <el-pagination
+      <div class="pt-10">加载中。。。</div>
+      <!-- <el-pagination
         background
         layout="prev, pager, next"
         :page-size="15"
         :total="pagination.total"
         class="mt-4"
         v-model:current-page="pagination.page"
-      />
+      /> -->
     </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-import ModelItemCard from "./ModelItemCard.vue";
+<script lang="ts" setup name="ModelHome">
+import ModelItemCard from "./ModelItemCard2.vue";
 import { modelList as modelListApi, watchModel } from "@/api/model";
 import { useUserStore } from "@/store/user";
 
@@ -71,66 +69,14 @@ type ModelList = {
 const modelList = ref<ModelList>([
   {
     picture:
-      "https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png",
+      "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/3c451176-a098-4191-907a-e898bfdf673b/width=2048/ComfyUI_00747_.jpeg",
     name: "原神 - 妮露DiffSinger模型",
     modelType: "[1,2,3,4]",
     types: [{ id: "1", type: "妮露" }],
     lookNum: 0,
     uploadDate: "222",
-    avatar: "https://uc.zhjlfx.cn/avatar.php?uid=2&size=middle",
-    nickname: "红血球AE3803",
-    mid: "22",
-    uid: "222",
-  },
-  {
-    picture:
-      "https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png",
-    name: "原神 - 妮露DiffSinger模型",
-    modelType: "[1,2,3,4]",
-    types: [{ id: "1", type: "妮露" }],
-    lookNum: 0,
-    uploadDate: "222",
-    avatar: "https://uc.zhjlfx.cn/avatar.php?uid=2&size=middle",
-    nickname: "红血球AE3803",
-    mid: "22",
-    uid: "222",
-  },
-  {
-    picture:
-      "https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png",
-    name: "原神 - 妮露DiffSinger模型",
-    modelType: "[1,2,3,4]",
-    types: [{ id: "1", type: "妮露" }],
-    lookNum: 0,
-    uploadDate: "222",
-    avatar: "https://uc.zhjlfx.cn/avatar.php?uid=2&size=middle",
-    nickname: "红血球AE3803",
-    mid: "22",
-    uid: "222",
-  },
-  {
-    picture:
-      "https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png",
-    name: "原神 - 妮露DiffSinger模型",
-    modelType: "[1,2,3,4]",
-    types: [{ id: "1", type: "妮露" }],
-    lookNum: 0,
-    uploadDate: "222",
-    avatar: "https://uc.zhjlfx.cn/avatar.php?uid=2&size=middle",
-    nickname: "红血球AE3803",
-    mid: "22",
-    uid: "222",
-  },
-  {
-    picture:
-      "https://fj.zhjlfx.cn/aibbs/forum/202303/15/204316lr8wvjths8im441m.png",
-    name: "原神 - 妮露DiffSinger模型",
-    modelType: "[1,2,3,4]",
-    types: [{ id: "1", type: "妮露" }],
-    lookNum: 0,
-    uploadDate: "222",
-    avatar: "https://uc.zhjlfx.cn/avatar.php?uid=2&size=middle",
-    nickname: "红血球AE3803",
+    avatar: "/img/user-placeholder.webp",
+    nickname: "RVC",
     mid: "22",
     uid: "222",
   },
@@ -164,26 +110,48 @@ const handleToDetail = (item) => {
   }
   router.push(`/model-detail/${item.mid}`);
 };
+
+onMounted(() => {
+  window.addEventListener("scroll", function () {
+    console.log(window.innerHeight, window.scrollY, document.body.offsetHeight);
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    }
+  });
+});
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.model-home {
+  :deep(.carousel-wrap .el-card__body) {
+    padding: 0;
+  }
+}
 .publish-btn {
+  width: 133px;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-left: 16px;
   min-width: 90px;
-  height: 34px;
+  height: 40px;
   border-radius: 8px;
 
-  background: var(--el-color-primary);
+  border: var(--el-border);
+  background-color: var(--el-button-bg-color);
+  color: var(--el-button-text-color);
 
-  color: #fff;
   text-align: center;
   font-size: 14px;
   line-height: 20px;
   cursor: pointer;
 
   padding: 0 10px;
+}
+
+.model-home-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  justify-content: flex-start;
 }
 </style>
