fe9c04d47f33e7aa473d1e7727a7512c0ced9fb2
Daniel Chen <9605239+danny8819@users.noreply.github.com>
2023-10-04 21:30:59 +0800

    修复富文本编辑器汉化bug以及调整创建文件页面样式。

--- src/pages/article/comps/CreateArticle.vue
+++ src/pages/article/comps/CreateArticle.vue
@@ -42,37 +42,6 @@
       <RichTextEditor />
     </div>
     <div class="w-1/3 px-1 py-24 flex flex-col gap-4">
-      <button
-        class="block w-full text-xl font-bold bg-white text-[#94A3B8] border-2 rounded-md py-2 hover:bg-opacity-50 dark:bg-[#1E293B]"
-      >
-        保存草稿
-      </button>
-      <button
-        class="block w-full text-xl font-bold border-2 rounded-md py-2 bg-blue-500 hover:bg-blue-600 text-white dark:text-white/80"
-      >
-        发布
-      </button>
-      <p class="text-sm">
-        您的文章当前
-        <a>处于隐藏状态</a>
-      </p>
-      <div class="flex gap-2">
-        <input type="checkbox" id="mature" />
-        <label for="mature" class="font-bold text-sm">
-          成熟
-          <span class="inline-flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 512 512"
-            >
-              <path
-                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
-              />
-            </svg>
-          </span>
-        </label>
-      </div>
       <div>
         <p class="text-xl font-bold inline-block pb-1 dark:text-[#94A3B8]">
           封面图片
@@ -168,6 +137,37 @@
           </svg>
         </button>
       </div>
+      <div class="flex gap-2">
+        <input type="checkbox" id="mature" />
+        <label for="mature" class="font-bold text-sm">
+          成熟
+          <span class="inline-flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+              />
+            </svg>
+          </span>
+        </label>
+      </div>
+      <button
+        class="block w-full text-xl font-bold bg-white text-[#94A3B8] border-2 rounded-md py-2 hover:bg-opacity-50 dark:bg-[#1E293B]"
+      >
+        保存草稿
+      </button>
+      <button
+        class="block w-full text-xl font-bold border-2 rounded-md py-2 bg-blue-500 hover:bg-blue-600 text-white dark:text-white/80"
+      >
+        发布
+      </button>
+      <p class="text-sm">
+        您的文章当前
+        <a>处于隐藏状态</a>
+      </p>
 
       <!-- <div>
         <p>附件</p>
